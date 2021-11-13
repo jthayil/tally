5f489110bf995592a7f21410280e5f2a1cd3c763
JThayil <jones.thayil@gmail.com>
2021-11-13 15:07:31 +0530

    new git

--- src/tally/serializers.py
+++ src/tally/serializers.py
@@ -1,15 +1,9 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
 from .models import Drs
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ["url", "username", "email", "is_staff"]
+class DrsModelSerializer(serializers.ModelSerializer):
 
-
-class DrsModelSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Drs
         exclude = ["inserted_On", "updated_On"]
--- src/tally/urls.py
+++ src/tally/urls.py
@@ -1,18 +1,29 @@
 """pvlin URL Configuration
-    path('', views.home, name='home')
-    path('', Home.as_view(), name='home')
-    path('blog/', include('blog.urls'))
-"""
-from django.urls import path, include
-from django.views.generic.base import View
-from tally import views
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from tally import views
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("drs_report/list/", views.drsreportlist, name="drsreportlist"),
-    path("drs_report/detail/<int:pk>/", views.drsreportdetail, name="drsreportdetail"),
+    path("drs_report/detail/<int:pk>/",
+         views.drsreportdetail, name="drsreportdetail"),
     path("drs_report/create/", views.drsreportcreate, name="drsreportcreate"),
-    path("drs_report/update/<int:pk>/", views.drsreportupdate, name="drsreportupdate"),
-    path("drs_report/delete/<int:pk>/", views.drsreportselete, name="drsreportselete"),
+    path("drs_report/update/<int:pk>/",
+         views.drsreportupdate, name="drsreportupdate"),
+    path("drs_report/delete/<int:pk>/",
+         views.drsreportselete, name="drsreportselete"),
 ]
